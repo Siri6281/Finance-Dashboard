c3eaf6f46135263211fc38b5b72f1be964b2c8d8
Gogula Siri chandana <16sirichandanag@gmail.com>
2026-04-06 19:35:41 +0530

    Add initial transactions data to data.js

---
+++ data.js
@@ -0,0 +1,13 @@
+const transactions = [
+    { date: "2026-04-01", amount: 500, category: "Food", type: "expense" },
+    { date: "2026-04-02", amount: 2000, category: "Salary", type: "income" },
+    { date: "2026-04-03", amount: 300, category: "Transport", type: "expense" },
+    { date: "2026-04-04", amount: 500, category: "Food", type: "expense" },
+    { date: "2026-04-05", amount: 1500, category: "Freelance", type: "income" },
+    { date: "2026-04-06", amount: 700, category: "Shopping", type: "expense" },
+    { date: "2026-04-07", amount: 800, category: "Tax", type: "expense" },
+    { date: "2026-04-08", amount: 1756, category: "Salary", type: "income" },
+    { date: "2026-04-09", amount: 875, category: "Groceries", type: "expense" },
+    { date: "2026-04-10", amount: 350, category: "Food", type: "expense" },
+    { date: "2026-04-11", amount: 1500, category: "Freelance", type: "income" },   
+];
